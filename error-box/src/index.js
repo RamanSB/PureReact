ae185e98d3b3e5d6705c8d3c2d294948c04d96aa
RamanSB <ramansb100@gmail.com>
2021-02-16 18:47:01 +0000

    Error box task complete

--- error-box/src/index.js
+++ error-box/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import warningLogoTest from './res/icons8-warning-triangle-64.png';
+import warningLogoTest from './res/icons8-brake-warning-64.png';
 import './index.css';
 
-
+//Can put the image in the public directory so the server can access it OR import.
 function ErrorBox(){
   return (
     <div className="error-box">
-      <img src="/icons8-warning-triangle-64.png"/>
+      <span id="span-text"><img id="error-img" src={warningLogoTest}/>The world is ending!</span>
     </div>
   );
 }
