30d28c6734ee7645b3452eb317e8491c3fc41222
RamanSB <ramansb100@gmail.com>
2021-02-16 18:54:33 +0000

    Added prop typesrequirement

--- error-box/src/index.js
+++ error-box/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import warningLogoTest from './res/icons8-brake-warning-64.png';
+import PropTypes from 'prop-types';
 import './index.css';
 
 //Can put the image in the public directory so the server can access it OR import.
@@ -12,5 +13,13 @@ function ErrorBox({children}){
   );
 }
 
+ErrorBox.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.string.isRequired
+  ])
+}
+
 let children = "The world is ending!";
+
+
 ReactDOM.render(<ErrorBox children={children}/>, document.querySelector('#root'));
