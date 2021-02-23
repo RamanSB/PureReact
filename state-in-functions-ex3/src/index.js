81c6e2aca0cd38fefa8b1783df8ecb5fc0816c0a
RamanSB <ramansb100@gmail.com>
2021-02-23 03:44:29 +0000

    Attempt at solving ex 3 functions with state

--- state-in-functions-ex3/src/index.js
+++ state-in-functions-ex3/src/index.js
@@ -2,15 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Decrementer(){
+function Decrementer({state_updater, component_name}){
+  console.log(`[Decrementer] component_name: ${component_name}`);
+  function handleDecrement(){
+    console.log(`[handleDecrement] component_name: ${component_name}`);
+    state_updater((prevState) => (
+      (prevState[component_name]-1 <= 0) ?
+         {
+          ...prevState,
+          [component_name]:50
+        }
+      :
+       {
+        ...prevState,
+        [component_name]:prevState[component_name]-1
+      }
+    ));
+  }
   return (
-    <button style={{marginRight:"28px"}}>-</button>
+    <button style={{marginRight:"28px"}} onClick={handleDecrement}>-</button>
   );
 }
 
-function Incrementer(){
+function Incrementer({state, state_updater, component_name}){
+  function handleIncrement(){
+    console.log(`[hanldeIncrement] state.component_name: ${state.component_name}`);
+    state_updater((prevState) => (
+      (prevState[component_name] >= 100) ?
+       {
+         ...prevState,
+         [component_name]: 50
+       }
+        :
+       {
+        ...prevState,
+        [component_name]: prevState[component_name]+1
+      }
+    ));
+
+    console.log(`[handleIncrement] after updater: ${JSON.stringify(state)}`);
+  }
+
   return (
-    <button style={{marginLeft:"28px"}}>+</button>
+    <button style={{marginLeft:"28px"}} onClick={handleIncrement}>+</button>
   );
 }
 
@@ -27,18 +61,18 @@ function SpeakerComponent({name}){
     volume: 89,
     bass: 41,
     mid: 50,
-    treble: 32
+    treble: 12
   };
   const [volumes, setVolumes] = React.useState(initState)
-
+  console.log(`[speakerComponent] volumes: ${JSON.stringify(volumes)}`);
   return (
     <div id="attribute-container">
-      <Decrementer/>
+      <Decrementer state_updater={setVolumes} component_name={name}/>
         <div>
           <SoundValue value={volumes[name]}/>
           <SoundAttribute>{name}</SoundAttribute>
         </div>
-      <Incrementer/>
+      <Incrementer state={volumes} state_updater={setVolumes} component_name={name}/>
     </div>
   );
 }
