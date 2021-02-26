2290558d5b71f99a5658621edb7a27265af80110
RamanSB <ramansb100@gmail.com>
2021-02-26 02:08:23 +0000

    Using reducer exercise 2

---
+++ use-reducer-ex2/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+const filePrefix = 'light-switch-icon-32-';
+const fileFormat = '.png'
+
+const reducer = (state, action) => {
+  let newLightLevel = (state+action);
+  if(newLightLevel>3){
+    newLightLevel = 0;
+  }
+  return newLightLevel;
+}
+
+function Room(){
+
+    const [lightLevel, dispatch] = React.useReducer(reducer, 0);
+
+    let imgSrcArray = [
+      filePrefix+"off"+fileFormat,
+      filePrefix+"dim"+fileFormat,
+      filePrefix+"lit"+fileFormat,
+      filePrefix+"bright"+fileFormat
+    ];
+
+    const handleClick = (event) => {
+      dispatch(1);
+    }
+
+    return (
+      <div className="container">
+      <h2>Light (usingReducer)</h2>
+        <img id="light-bulb" src={imgSrcArray[lightLevel]}/>
+        <br/>
+        <button onClick={handleClick} style={{width:"168px", height: "48px", marginTop:"16px", border:"4px dotted black", borderRadius:"16px"}}>LightSwitch</button>
+      </div>
+    )
+}
+
+
+ReactDOM.render(<Room/>, document.querySelector('#root'));
