dbac3dbf56558f311e28aa1c412b924c2dc23f07
RamanSB <ramansb100@gmail.com>
2021-03-04 17:43:54 +0000

    Initial UI - Cart & Items on Nav bar & empty content

---
+++ shopper/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Nav from './Nav';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Nav/>
+      <main className="App-content">
+        <span>Empty</span>
+      </main>
+    </div>
+  );
+};
+
+
+
+export default App;
---
+++ shopper/src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => (
+  <nav className="App-nav">
+    <ul>
+      <li className="App-nav-item">
+        <button>Items</button>
+      </li>
+      <li className="App-nav-item">
+        <button>Cart</button>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Nav;
