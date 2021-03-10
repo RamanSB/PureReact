d947d0b3068ea04d1daaf7bd1c13816dc3a8e7b8
RamanSB <ramansb100@gmail.com>
2021-03-10 16:03:09 +0000

    Cart Page created, now to modify items to accept child prop to differentiate between regular Item or CartItem

--- shopper/src/App.js
+++ shopper/src/App.js
@@ -1,18 +1,66 @@
 import React from 'react';
 import Nav from './Nav';
 import './App.css';
+import ItemPage from './ItemPage';
+import CartPage from './CartPage';
+import { items } from './static-data';
+
+
+function summarizeCart(cart){
+  const groupedItems = cart.reduce((summary, item) => {
+    console.log(`Summary: ${JSON.stringify(summary)}\nItem; ${JSON.stringify(item)}`);
+    summary[item.id] = summary[item.id] || {
+      ...item,
+      count:0
+    };
+
+    summary[item.id].count++;
+    return summary;
+  }, {});
+
+  return Object.values(groupedItems);
+}
 
 const App = () => {
+
+  const [activeTab, setActiveTab] = React.useState('items');
+  const [cart, setCart] = React.useState([]);
+
+  const addToCart = (item) => {
+    setCart(prevCart => [...prevCart, item]);
+    console.log(`[AddToCart] ${JSON.stringify(cart)}`);
+  };
+
+
+
   return (
     <div className="App">
-      <Nav/>
+      <Nav
+        activeTab={activeTab}
+        onTabChange={setActiveTab}
+      />
       <main className="App-content">
-        <span>Empty</span>
+        <Content onAddToCart={addToCart} tab={activeTab} cart={summarizeCart(cart)}/>
       </main>
     </div>
   );
 };
 
+
+const Content = ({onAddToCart, tab, cart}) => {
+  console.log(`Tab: ${JSON.stringify(tab)}`);
+  switch(tab){
+    default:
+    case 'items':
+      return (
+        <ItemPage
+          items={items}
+          onAddToCart={onAddToCart}/>
+      );
+    case 'cart':
+      return <CartPage items={cart}/>;
+  }
+};
 
 
 export default App;
--- shopper/src/Nav.js
+++ shopper/src/Nav.js
@@ -1,16 +1,34 @@
 import React from 'react'
 
-const Nav = () => (
-  <nav className="App-nav">
-    <ul>
-      <li className="App-nav-item">
-        <button>Items</button>
-      </li>
-      <li className="App-nav-item">
-        <button>Cart</button>
-      </li>
-    </ul>
-  </nav>
-);
+const Nav = ({activeTab, onTabChange}) => {
+
+    console.log(`[Nav] activeTab: ${activeTab}`);
+
+    const itemClass = (tabName) => {
+      return (`App-nav-item ${
+        activeTab === tabName ? 'selected' : ''
+      }`);
+    };
+
+    const NavItem = ({itemName, children}) => {
+      return (
+        <li className={itemClass(itemName)}>
+          <button onClick={() => {
+              onTabChange(itemName);
+            }
+          }>{children}</button>
+        </li>
+      );
+    }
+
+    return (
+      <nav className="App-nav">
+        <ul>
+          <NavItem itemName="items">Items</NavItem>
+          <NavItem itemName="cart">Cart</NavItem>
+        </ul>
+      </nav>
+  );
+}
 
 export default Nav;
